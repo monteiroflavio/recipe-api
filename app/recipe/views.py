179b49849dc691219da9fc16d3fbf8405482cefc
sarabada <fm070795@gmail.com>
2020-03-31 14:06:33 -0300

    adding create ingredient endpoint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -23,7 +23,9 @@ class TagViewSet(viewsets.GenericViewSet,
         serializer.save(user=self.request.user)
 
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     """Manages ingredients in the database."""
 
     authentication_classes = (TokenAuthentication, )
@@ -34,3 +36,7 @@ class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Returns objects for the current authenticated user."""
         return self.queryset.filter(user=self.request.user). order_by('-name')
+
+    def perform_create(self, serializer):
+        """Creates a new ingredient."""
+        serializer.save(user=self.request.user)
