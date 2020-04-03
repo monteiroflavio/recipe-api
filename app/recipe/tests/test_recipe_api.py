be63b67af26b51d92680156063f4e64a48f14529
sarabada <fm070795@gmail.com>
2020-04-03 14:13:45 -0300

    adding ingredients and tags filters to recipe get endpoint

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -251,3 +251,49 @@ class ImageUploadTest(TestCase):
         res = self.client.post(url, {'image': 'notimage'}, format='multipart')
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_filter_recipe_filter_recipe_by_tag(self):
+        """Tests returning recipes with specific tags."""
+        recipe1 = sample_recipe(user=self.user, title='title 1')
+        recipe2 = sample_recipe(user=self.user, title='title 2')
+        tag1 = sample_tag(user=self.user, name='tag 1')
+        tag2 = sample_tag(user=self.user, name='tag 2')
+        recipe1.tags.add(tag1)
+        recipe2.tags.add(tag2)
+        recipe3 = sample_recipe(user=self.user, title='title 3')
+
+        res = self.client.get(
+            RECIPE_URLS,
+            {'tags': f'{tag1.id}, {tag2.id}'}
+        )
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
+
+    def test_filter_recipes_by_ingredients(self):
+        """Tests returning recipes with specific ingredients."""
+        recipe1 = sample_recipe(user=self.user, title='title 1')
+        recipe2 = sample_recipe(user=self.user, title='title 2')
+        ingredient1 = sample_ingredient(user=self.user, name='ingredient 1')
+        ingredient2 = sample_ingredient(user=self.user, name='ingredient 2')
+        recipe1.ingredients.add(ingredient1)
+        recipe2.ingredients.add(ingredient2)
+        recipe3 = sample_recipe(user=self.user, title='title 3')
+
+        res = self.client.get(
+            RECIPE_URLS,
+            {'ingredients': f'{ingredient1.id}, {ingredient2.id}'}
+        )
+
+        serializer1 = RecipeSerializer(recipe1)
+        serializer2 = RecipeSerializer(recipe2)
+        serializer3 = RecipeSerializer(recipe3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
